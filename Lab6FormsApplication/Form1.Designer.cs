75fadf55479d8f11771dfa32c7bcee9efeb0b044
M-at-symbol <mrc17@pct.edu>
2016-02-14 17:52:06 -0500

    Initial Commit

---
+++ Lab6FormsApplication/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Lab6FormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.incButton = new System.Windows.Forms.Button();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Resistance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OnTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.capValue = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // incButton
+            // 
+            this.incButton.Location = new System.Drawing.Point(44, 29);
+            this.incButton.Name = "incButton";
+            this.incButton.Size = new System.Drawing.Size(75, 23);
+            this.incButton.TabIndex = 0;
+            this.incButton.Text = "Start";
+            this.incButton.UseVisualStyleBackColor = true;
+            this.incButton.Click += new System.EventHandler(this.incButton_Click);
+            // 
+            // serialPort2
+            // 
+            this.serialPort2.BaudRate = 115200;
+            this.serialPort2.PortName = "COM4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Resistance,
+            this.OnTime});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 157);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(671, 274);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Resistance
+            // 
+            this.Resistance.HeaderText = "Resistance";
+            this.Resistance.Name = "Resistance";
+            // 
+            // OnTime
+            // 
+            this.OnTime.HeaderText = "OnTime";
+            this.OnTime.Name = "OnTime";
+            // 
+            // capValue
+            // 
+            this.capValue.BackColor = System.Drawing.SystemColors.Window;
+            this.capValue.FormattingEnabled = true;
+            this.capValue.Items.AddRange(new object[] {
+            "None",
+            "1uF",
+            "2.2uF",
+            "3.3uF",
+            "4.7uF",
+            "10uF"});
+            this.capValue.Location = new System.Drawing.Point(298, 45);
+            this.capValue.Name = "capValue";
+            this.capValue.Size = new System.Drawing.Size(120, 82);
+            this.capValue.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Capcitor Selection";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Group ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Board ID";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(457, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(457, 97);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(563, 58);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(563, 97);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "MinResistance";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(454, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "MinOnTime";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(560, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "MaxResistance";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(560, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "MaxOnTime";
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(843, 447);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 17;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(679, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Upload Manaul Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(930, 482);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.capValue);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.incButton);
+            this.Name = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+   
+        private System.Windows.Forms.Button incButton;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox capValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Resistance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OnTime;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab6FormsApplication/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using NI = NationalInstruments.NI4882;
+
+namespace Lab6FormsApplication
+{
+    public partial class Form1 : Form
+    {
+        DMM dmm1 = new DMM(1); 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+            {
+                this.dataGridView1.Rows.Add(); //create table in the form
+            }   
+        }
+
+        private void incButton_Click(object sender, EventArgs e)
+        {
+            List<double> dmm1Res = new List<double>();
+
+                    for (int i = 0; i < 100; i++) 
+                    {
+                        dmm1Res.Add(dmm1.measureResistance()); 
+                        this.dataGridView1.Rows[i].Cells[0].Value = dmm1Res[i]; 
+                        serialPort2.WriteLine("i");
+                        Thread.Sleep(500); 
+                    }
+            
+        }
+       
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort2.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort2.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort2.WriteLine("d");
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            var boardId = textBox1.Text;
+            char groupId = Convert.ToChar(textBox2.Text);
+            var cap = getCap();
+            /* Grab data from spreadsheet and place in lists */
+            for (int i = 0; i < 100; i++)
+            {
+
+                var resistance = Convert.ToSingle(dataGridView1.Rows[i].Cells[0].Value);
+                var onTime = Convert.ToSingle(dataGridView1.Rows[i].Cells[1].Value);
+
+                var res = new EET321_Lab6DataContext();
+                var data = new EET321_Lab6_Table();
+
+
+                data.GroupID = groupId;
+                data.BoardID = boardId;
+                data.DateTime = DateTime.Now;
+                data.Resistance = resistance;
+                data.OnTime = onTime;
+                data.Capacitance = cap;
+
+                res.EET321_Lab6_Tables.InsertOnSubmit(data);//insert data to server
+                res.SubmitChanges(); //submit changes
+            }
+
+       
+        }
+
+        private float getCap()
+        {
+            var capStr = capValue.Text;
+            var capacitance = 0F;
+            switch (capStr)
+            {
+                case "1uF":
+                    capacitance = 1e-6F;
+                    break;
+                case "2.2uF":
+                    capacitance = 2.2e-6F;
+                    break;
+                case "3.3uF":
+                    capacitance = 3.3e-6F;
+                    break;
+                case "4.7uF":
+                    capacitance = 4.7e-6F;
+                    break;
+                case "10uF":
+                    capacitance = 10e-6F;
+                    break;
+            }
+            return capacitance;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+ 
+}
+
+    /* Thanks Jonny */
+    class DMM
+    {
+        NI.Device device;
+        //Initialize the DMM with an address
+        public DMM(byte addr)
+        {
+            device = new NI.Device(0, addr);
+        }
+
+        //Does exactly what you think it should
+        public double measureResistance()
+        {
+            device.Write("MEAS:RES?"); 
+            var str = device.ReadString(); //reads value from DMM
+            double d = 0.0;
+
+            /* If it cannot parse the str then try and measure again */
+            try
+            {
+                d = double.Parse(str);
+            }
+            catch
+            {
+                //weird error look at dmm
+                d = this.measureResistance();
+            }
+            return d;
+        }
+    }
+
+
